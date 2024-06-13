1da4c9498cb67bbdd877fbcf35ff9cf40c5038be
ishaan19d <ishaandas848@gmail.com>
2024-06-13 21:33:52 +0530

    Initial Commit

---
+++ OTTShowQuotes/OTTShowQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTTShowQuotesApp.swift
+//  OTTShowQuotes
+//
+//  Created by Ishaan Das on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct OTTShowQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
